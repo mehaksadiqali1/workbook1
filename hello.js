ad1a0a7a17bb9aa732fda9fcbd5582a8047d15c9
Mehak Sadiq Ali <sadiqalimehak@gmail.com>
2024-10-08 11:16:01 -0400

    hello java

---
+++ hello.js
@@ -0,0 +1,9 @@
+
+//Example 1: decalre and assign separately
+var message ; //declaration
+message= "Programming is fun!"; //assignment
+
+//Example 2;
+//declare and assign on the same line
+var message="Programming is fun!";
+console.log(message);
---
+++ strict-mode.js
@@ -0,0 +1,5 @@
+"use strict";
+var number;
+number=10;
+numbers=12;
+console.log(number);
